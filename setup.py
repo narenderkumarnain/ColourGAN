d13cca7edd4b1dee8ab06cc662ddc6637fb32c2c
Narender Nain <narender.nain@siemens.com>
2021-06-26 13:56:54 +0530

    Initial working code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="colourgan",
-    version="1.0.1",
+    version="1.0.2",
     author="Narender Nain",
     author_email="narenderkumarnain@gmail.com",
     description="GAN Based colouring model",
